a79b63b6b0b557cbc5d81019bec6f18639321929
Philipe Anderson De Campos <philipeacampos@hotmail.com>
2020-03-26 00:21:16 -0300

    Replementando GIT

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP JEITO CERTO</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <?php
+        $start = "Vamos Começar!";
+        echo "<h1>Olá Mundo! {$start}</h1>";
+        echo "<div id='js'>Loading...</div>"
+
+    ?>
+    <script src="assets/scripts.js"></script>
+</body>
+</html>
